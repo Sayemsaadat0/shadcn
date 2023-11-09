e755d83a1fbf0e6790dcfee32b1ae4200101abaf
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-09 17:15:45 +0600

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { AccordionDemo } from "@/components/core/accordion";
+import Image from "next/image";
 
 export default function Home() {
   return (
- <main>main </main>
-  )
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <div className="w-96">
+        <AccordionDemo></AccordionDemo>
+      </div>
+    </main>
+  );
 }
