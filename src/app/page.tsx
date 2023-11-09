d844ed057c9c916f994061f71ba9bd8938a6790e
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-09 18:05:40 +0600

    some customized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import { AccordionDemo } from "@/components/core/accordion";
+import { DialogDemo } from "@/components/core/dialog";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
+
+      <h1 className="text-2xl font-mono font-bold underline">ShadCn Learning</h1>
       <div className="w-96">
-        <AccordionDemo></AccordionDemo>
+        accordion ::::
+        <AccordionDemo></AccordionDemo> <br /> <br />
+      </div>
+
+      <div className="">
+       <p className="bg-tm-Primary-2 text-white text-center"> dialoge :</p> <br />
+        <DialogDemo></DialogDemo>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,41 +16,6 @@ module.exports = {
       },
     },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -70,6 +35,54 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        'tm-Primary-1': '#F0EBFA',
+        'tm-Primary-2': '#CFBBF4',
+        'tm-Primary-3': '#B498E9',
+        'tm-Primary-4': '#A688E1',
+        'tm-Primary-5': '#976FE5',
+        'tm-Primary-6': '#7F4FDA',
+        'tm-Primary-7': '#6935CC',
+        'tm-Primary-8': '#3C08A0',
+
+        'tm-Secondary-1': '#FFDEEC',
+        'tm-Secondary-2': '#F8A5C9',
+        'tm-Secondary-3': '#F68FBC',
+        'tm-Secondary-4': '#F479AF',
+        'tm-Secondary-5': '#F363A2',
+        'tm-Secondary-6': '#F14C94',
+        'tm-Secondary-7': '##EF3687',
+        'tm-Secondary-8': '#ED1F79',
+         
+        'tm-Neutral-1': '#FFFFFF',
+        'tm-Neutral-2': '#FAF5F5',
+        'tm-Neutral-3': '#E2E3FF',
+        'tm-Neutral-4': '#777777',
+        'tm-Neutral-5': '#10022D',
+        
+        'tm-Cheap-1': '#E2E2FF',
+        'tm-Cheap-2': '#F4EFFC',
+        'tm-Cheap-3': '#FED2E5',
+        'tm-Cheap-4': '#D5E8FA',
+        'tm-Cheap-5': '#F4F1D2',
+        'tm-Cheap-6': '#AE85FD',       
+        'tm-Cheap-7': '#CEECF7',
+        'tm-Cheap-8': '#FAF5F5',
+        'tm-Cheap-9': '#D3E1FD',
+     
+        Error: {
+          DEFAULT: '#ED1F79',
+        },
+        Success: {
+          DEFAULT: '#15AB24',
+        },
+        Disable: {
+          DEFAULT: '#E3E3E3',
+        },
+        Grey: {
+          DEFAULT: '#A4A4A4',
+        },
+      }
     },
   },
   plugins: [require("tailwindcss-animate"),require("daisyui")],
