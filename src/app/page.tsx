81507ca5811f6ca442a6528fe6f0fe70026a8d03
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-09 18:34:53 +0600

    now complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { AccordionDemo } from "@/components/core/accordion";
 import { DialogDemo } from "@/components/core/dialog";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,14 +8,15 @@ export default function Home() {
     <main className="min-h-screen flex flex-col justify-center items-center">
 
       <h1 className="text-2xl font-mono font-bold underline">ShadCn Learning</h1>
-      <div className="w-96">
-        accordion ::::
-        <AccordionDemo></AccordionDemo> <br /> <br />
-      </div>
+
 
       <div className="">
        <p className="bg-tm-Primary-2 text-white text-center"> dialoge :</p> <br />
         <DialogDemo></DialogDemo>
+      </div>
+
+      <div className="py-20">
+       <Input className="" placeholder="shshshs"></Input>
       </div>
     </main>
   );
